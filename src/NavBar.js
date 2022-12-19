379df13f04e132f99f18f0e50f676cbe9cf94392
Minchul An <min@minchulan.io>
2022-12-19 15:11:03 -0500

    added in 2 more client-side routes -- /search and /new-drug

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,6 +6,8 @@ const NavBar = () => {
     return (
         <nav>
             <Link to="/">Home</Link>
+            <Link to="/search">Search</Link>
+            <Link to="/new-drug">New Drug</Link>
         </nav>
     );
 }
